2f1eebc75cc8eba52c6f5c16bb0c5f3d733ff1d0
oliveirahelo <heloisa.oli15@gmail.com>
2025-05-22 15:43:58 -0300

    Add files via upload

---
+++ while.py
@@ -0,0 +1,7 @@
+numero = int(input(" Digite seu numero: "))
+comeco = int(input("Digite o numero que devera começar: "))
+fim = int(input(" Digite ate qual numero deverar ser multiplicado: "))
+ 
+while comeco <= (fim):
+    print(f" {comeco} x {numero} = {comeco * numero}")
+    comeco+=1
